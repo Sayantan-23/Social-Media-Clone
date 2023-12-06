31f5798141d3daf36c3e8edc34c7c7239d8af1fa
Sayantan <sayantan358@gmail.com>
2023-12-06 21:31:11 +0530

    user header component design done

---
+++ client/src/components/UserHeader.jsx
@@ -0,0 +1,110 @@
+import {
+  VStack,
+  Flex,
+  Box,
+  Avatar,
+  Text,
+  Link,
+  Menu,
+  MenuButton,
+  Portal,
+  MenuList,
+  MenuItem,
+  useToast,
+} from "@chakra-ui/react";
+import { BsInstagram } from "react-icons/bs";
+import { CgMoreO } from "react-icons/cg";
+
+const UserHeader = () => {
+  const toast = useToast();
+
+  const copyUrl = () => {
+    const currentUrl = window.location.href;
+    navigator.clipboard.writeText(currentUrl).then(() => {
+      toast({
+        description: "Copied to clipboard",
+        status: "success",
+        duration: "2000",
+        isClosable: "true",
+      });
+    });
+  };
+
+  return (
+    <VStack gap={4} alignItems="start">
+      <Flex justifyContent="space-between" w="full">
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Avatar 1
+          </Text>
+          <Flex gap={2} alignItems="center">
+            <Text fontSize="sm">avatar1</Text>
+            <Text
+              fontSize="xs"
+              bg={"gray.dark"}
+              color={"gray.light"}
+              p={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          <Avatar name="Avatar 1" src="/avatar-1.jpg" size="xl" />
+        </Box>
+      </Flex>
+
+      <Text>CEO, Executive, Dictator</Text>
+      <Flex w={"full"} justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text color={"gray.light"}>3.2k followers</Text>
+          <Box w={1} h={1} bg={"gray.light"} borderRadius={"full"}></Box>
+          <Link color={"gray.light"}>instagram.com</Link>
+        </Flex>
+        <Flex>
+          <Box className="icon-container">
+            <BsInstagram size={24} cursor={"pointer"} />
+          </Box>
+          <Box className="icon-container">
+            <Menu>
+              <MenuButton>
+                <CgMoreO size={24} cursor={"pointer"} />
+              </MenuButton>
+              <Portal>
+                <MenuList bg={"gray.dark"}>
+                  <MenuItem bg={"gray.dark"} onClick={copyUrl}>
+                    Copy Link
+                  </MenuItem>
+                </MenuList>
+              </Portal>
+            </Menu>
+          </Box>
+        </Flex>
+      </Flex>
+      <Flex w={"full"}>
+        <Flex
+          flex={1}
+          borderBottom={"1.5px solid white"}
+          justifyContent={"center"}
+          pb={3}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Threads</Text>
+        </Flex>
+        <Flex
+          flex={1}
+          borderBottom={"1px solid gray"}
+          justifyContent={"center"}
+          color={"gray.light"}
+          pb={3}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Replies</Text>
+        </Flex>
+      </Flex>
+    </VStack>
+  );
+};
+
+export default UserHeader;
