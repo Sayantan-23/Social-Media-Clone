5aeda60aa3c53428ba47569e0f2e2c8ff2e5508f
Sayantan <sayantan358@gmail.com>
2023-12-07 01:56:26 +0530

    userPost basic design done

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,9 +1,14 @@
 import UserHeader from "../components/UserHeader"
+import UserPost from "../components/UserPost"
 
 const UserPage = () => {
   return (
     <>
       <UserHeader />
+      <UserPost likes={851} replies={245} postImage="/post-1.avif" postTitle="It is my first post" />
+      <UserPost likes={124} replies={45} postImage="/post-2.avif" postTitle="It is my first post" />
+      <UserPost likes={325} replies={125} postImage="/post-3.avif" postTitle="It is my first post" />
+      <UserPost likes={1451} replies={345} postImage="/post-4.avif" postTitle="It is my first post" />
     </>
   )
 }
